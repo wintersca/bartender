b8ed01b951d23e348bd61b9a57765c9807bf0624
silverSapphire <u0844864@utah.edu>
2018-11-22 22:52:39 -0700

    Game loop (onUpdate) detects when left button is held down over sprite.

--- mycanvas.cpp
+++ mycanvas.cpp
@@ -9,12 +9,28 @@ void MyCanvas::OnInit()
     mySprite.setTexture(myTexture);
 }
 
+//Game loop
 void MyCanvas::OnUpdate()
 {
     // Clear screen
-    sf::RenderWindow::clear(sf::Color(0, 180, 0));
+    clear(sf::Color(0, 180, 0));
 
-    mySprite.move(.01, .01);
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        // left click...
+        //qDebug() << "You clicked the left button!";
+        sf::Vector2i mouse = sf::Mouse::getPosition(*this);
+        //qDebug() << "The mouse coordinates are at " << mouse.x << ", " << mouse.y;
+
+        if(mySprite.getGlobalBounds().contains(sf::Vector2f(mouse)))
+               qDebug() << "Mouse is on Sprite!";
+
+    }
+
+    mySprite.move(.0001f, .0001f);
+
+    sf::Vector2f sprite = mySprite.getPosition();
+    //qDebug() << "The sprite coordinates are at " << sprite.x << ", " << sprite.y;
 
     draw(mySprite);
 }
