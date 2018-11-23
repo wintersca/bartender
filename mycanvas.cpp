4096ce2250a2c4163bd5617fc285bfd6ecb12d7b
silverSapphire <u0844864@utah.edu>
2018-11-22 22:07:17 -0700

    Moving sprite by fixed amount programatically!

--- mycanvas.cpp
+++ mycanvas.cpp
@@ -1,3 +1,20 @@
 #include "mycanvas.h"
 
+void MyCanvas::OnInit()
+{
 
+    // Setup the sprite
+
+    myTexture.loadFromFile("cherry.png");
+    mySprite.setTexture(myTexture);
+}
+
+void MyCanvas::OnUpdate()
+{
+    // Clear screen
+    sf::RenderWindow::clear(sf::Color(0, 180, 0));
+
+    mySprite.move(.01, .01);
+
+    draw(mySprite);
+}
--- mycanvas.h
+++ mycanvas.h
@@ -10,45 +10,22 @@
 
 class MyCanvas : public QSFMLCanvas
 {
+
 public :
 
     MyCanvas(QWidget* Parent, const QPoint& Position, const QSize& Size) :
-    QSFMLCanvas(Parent, Position, Size)
-    {
-
-    }
+        QSFMLCanvas(Parent, Position, Size) { }
 
 private :
-
-    void OnInit()
-    {
-
-        // Setup the sprite
-
-        myTexture.loadFromFile("cherry.png");
-        mySprite.setTexture(myTexture);
-
-//        sf::Texture texture;
-//        if(!texture.loadFromFile("cherry.png"))
-//               qDebug() << "Failed to load the lemon!";
-
-//        texture.setSmooth(true);
-//        mySprite.setTexture(texture);
-
-//        sf::IntRect rect = mySprite.getTextureRect();
-//        mySprite.setOrigin(rect.width, rect.height);
-    }
-
-    void OnUpdate()
-    {
-        // Clear screen
-        sf::RenderWindow::clear(sf::Color(0, 180, 0));
-        draw(mySprite);
-    }
 
     sf::Image  myImage;
     sf::Sprite mySprite;
     sf::Texture myTexture;
+
+    void OnInit();
+
+    void OnUpdate();
+
 };
 
 #endif // MYCANVAS_H
