0f0dfb291aeba435c6abf90962bf6c833d1009a3
Tristan Bowler <tristan.bowler@gmail.com>
2018-12-02 21:53:14 -0700

    fix duplication issue

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 int main(int argc, char *argv[])
 {
     XMLDrinkParser* xml = new XMLDrinkParser();
-    xml->parseXMLDatabase();
     QApplication a(argc, argv);
     Controller *c = new Controller(xml);
     MainWindow w(c);
